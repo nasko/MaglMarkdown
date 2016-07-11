44bcdd887c16dac8eba0931e7ec7f6e69dda18ea
Atanas Vasilev <a.vasilev@viscomp.bg>
2016-07-11 16:33:28 +0300

    migrate from psr-0 to psr-4

--- src/Module.php
+++ src/Module.php
@@ -34,18 +34,7 @@ class Module
 
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__,
-                ),
-            ),
-        );
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getViewHelperConfig()
